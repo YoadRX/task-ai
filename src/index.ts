ee84dfc0c3542d73aac4730ab419c366e01bfabb
YoadRX <yoadrimok@hilma.tech>
2025-02-16 15:13:52 +0200

    add type

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@
 export * from './llm-talker/llm-talker.module';
 export * from './llm-talker/llm-talker.service';
 export * from './types/LLM.t';
+export * from './types/FileType.t';
 export * from './uvr/uvr.service';
