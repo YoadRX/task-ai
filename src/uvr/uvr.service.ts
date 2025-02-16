ef18532b0e73542ee0429b12d1332fd491fd58e1
YoadRX <yoadrimok@hilma.tech>
2025-02-16 15:09:11 +0200

    add all

--- src/uvr/uvr.service.ts
+++ src/uvr/uvr.service.ts
@@ -7,22 +7,46 @@ import { FileType } from './uvr.controller';
 export class UvrService {
   constructor() {}
 
-  executeUvr(file: FileType) {
-    const filePath = './audio/test.wav';
+  executeUvr(file: FileType): Promise<{ noVocals: string; vocals: string }> {
+    return new Promise((resolve, reject) => {
+      const fileName = file.originalname.split('.')[0];
+      const filePath = `./audio/${file.originalname}`;
 
-    // Save the uploaded file
-    fs.writeFileSync(filePath, file.buffer);
+      // Save the uploaded file
+      fs.writeFileSync(filePath, file.buffer);
 
-    // Correct Docker command
-    const command = `DOCKER_CLI_HINTS=false docker --log-level debug run --rm -v "$(pwd)"/audio:/input -v "$(pwd)"/audio:/output -v "$(pwd)/uvr-cli-use/":/app uvr-cli-use /input/test.wav -m /app/models/MDX_Net_Models/UVR_MDXNET_3_9662.onnx -o /output`;
+      // Correct Docker command
+      const command = `DOCKER_CLI_HINTS=false docker --log-level debug run --rm -v "$(pwd)"/audio:/input -v "$(pwd)"/audio:/output -v "$(pwd)/uvr-cli-use/":/app uvr-cli-use /input/${file.originalname} -m /app/models/MDX_Net_Models/UVR_MDXNET_3_9662.onnx -o /output`;
 
-    exec(command, (error, stdout, stderr) => {
-      if (error) {
-        console.error(`Exec error: ${error.message}`);
-        return;
-      }
-      console.log(`stdout: ${stdout}`);
-      console.error(`stderr: ${stderr}`);
+      // Loading animation
+      const loadingAnimation = ['|', '/', '-', '\\'];
+      let i = 0;
+      const loadingInterval = setInterval(() => {
+        process.stdout.write(
+          `\rProcessing ${loadingAnimation[i++ % loadingAnimation.length]}`,
+        );
+      }, 200);
+
+      exec(command, (error, stdout, stderr) => {
+        clearInterval(loadingInterval); // Stop the loading animation
+        process.stdout.write('\rProcessing complete!        \n'); // Clear the animation line
+
+        if (error) {
+          console.error(`Exec error: ${error.message}`);
+          reject(new Error(error.message));
+          return;
+        }
+        console.log(`stdout: ${stdout}`);
+        console.error(`stderr: ${stderr}`);
+        resolve({
+          noVocals: fs
+            .readFileSync(`./audio/${fileName}_no_vocals.wav`)
+            .toString('base64'),
+          vocals: fs
+            .readFileSync(`./audio/${fileName}_vocals.wav`)
+            .toString('base64'),
+        });
+      });
     });
   }
 }
