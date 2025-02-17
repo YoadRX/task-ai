deff7cbb54225ea10bfab4188e82f6b19b4a94aa
YoadRX <yoadrimok@hilma.tech>
2025-02-17 10:49:16 +0200

    export format

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 // index.ts
+export * from './gemini-filer/gemini-filer.module';
+export * from './gemini-filer/gemini-filer.service';
 export * from './llm-talker/llm-talker.module';
 export * from './llm-talker/llm-talker.service';
 export * from './types/LLM.t';
 export * from './types/FileType.t';
-export * from './gemini-filer/gemini-filer.module';
-export * from './gemini-filer/gemini-filer.service';
+export * from './types/GenerateLLMResponse.t';
