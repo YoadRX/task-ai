0338161ff73081a706f586c360fce42acff7acf2
YoadRX <yoadrimok@hilma.tech>
2025-02-20 11:10:22 +0200

    deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,9 @@ import { LlmTalkerService } from './llm-talker/llm-talker.service';
 import { LlmTalkerModule } from './llm-talker/llm-talker.module';
 import { GeminiFilerController } from './gemini-filer/gemini-filer.controller';
 import { GeminiFilerModule } from './gemini-filer/gemini-filer.module';
-import { LlmTalkerController } from './llm-talker/llm-talker.controller';
 @Module({
   imports: [LlmTalkerModule, GeminiFilerModule],
-  controllers: [AppController, GeminiFilerController, LlmTalkerController],
+  controllers: [AppController, GeminiFilerController],
   providers: [LlmTalkerService],
 })
 export class AppModule {}
